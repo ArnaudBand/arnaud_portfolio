dc94c604ade5b4e912e5b32bbfd2509ece66c078
Arnaud <bandonkeyea@gmail.com>
2022-04-04 16:30:43 -0600

    Add js file

---
+++ script.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-plusplus */
+const btnMenu = document.querySelector('.btnMenu');
+const rightSide = document.querySelector('.right-side');
+const topLink = document.querySelectorAll('.top-link');
+const closeMenu = document.querySelector('.closeMenu');
+
+btnMenu.addEventListener('click', () => {
+  rightSide.style.display = 'flex';
+  for (let i = 0; i < topLink.length; i++) {
+    topLink[i].style.display = 'block';
+  }
+});
+
+closeMenu.addEventListener('click', () => {
+  rightSide.style.display = 'none';
+  for (let i = 0; i < topLink.length; i++) {
+    topLink[i].style.display = 'none';
+  }
+});
